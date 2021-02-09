8c82200940d3f15e623af43b735f14b4d0801131
Sarthakhanna0300 <sarthaksk6@gmail.com>
2021-02-09 15:40:33 +0530

    React google login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import GoogleLogin from 'react-google-login';
+import googleClientId from './config.js';
 
 function App() {
+  const responseSuccessGoogle = (resposne) =>{
+
+  }
+  const responseErrorGoogle = (resposne) =>{
+    
+  }
   return (
     <div className="App">
       <h1 className="text-center text-uppercase"> Hello! </h1>
+    
+      <GoogleLogin
+        clientId={googleClientId}
+        buttonText="Login"
+        onSuccess={responseSuccessGoogle}
+        onFailure={responseErrorGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
