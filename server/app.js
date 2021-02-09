e0ff92f860bcb6583c59b7dabe76e854cc2a8f09
tapixhx <tapish818@gmail.com>
2021-02-09 16:26:09 +0530

    Routes

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose  =require('mongoose');
 
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/auth', authRoutes);
 
-app.listen(8080);
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-wpluf.mongodb.net/${process.env.MONGO_DEFAULT_DB}?retryWrites=true&w=majority`
+).then(result => {
+    app.listen(8080);
+})
+.catch(err => console.log(err),)
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,74 +1,83 @@
 const { validationResult } = require('express-validator');
+const {OAuth2Client} = require('google-auth-library');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-exports.login = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    let loadedUser;
-    User.findOne({ email: email })
-    .then(user => {
-        if(!user) {
-            const error = new Error('User already exist');
-            error.statusCode(401);
-            throw error;
-        }
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
 
-        if(!user.isVerified) {
-            console.log('1233445566677787');
-            const otp = Math.floor(1000 + Math.random() * 9000).toString();
-            console.log(otp);
-
-            Otp.findOneAndRemove({ userId: user.id }, err => next(err));
-            console.log(user);
-            const userOtp = new Otp({
-                otp: otp,
-                userId: user._id
-            })
-            userOtp.save();
-
-            transporter.sendMail ({
-                to: email,
-                from: 'eventsity@india.com',
-                subject: 'Welcometo eventsity! Confirm your email',
-                html: `<h1>Thanks for signing up with Eventsity</h1>
-                        <h4>Here is your otp - ${otp}</h4>`
-            });
-
-            setTimeout(() => {
-                Otp.findByIdAndRemove(userOtp._id, err => next(err))
-            }, 200000);
-
-            console.log(user.id);
-            const error = new Error("User is not verified")
-            error.userId = user.id;
-            error.statusCode = 401;
-            throw error; 
-        }
-
-        loadedUser = user;
-        return bcrypt.compare(password, user.password);
+exports.gLogin = (req, res, next) => {
+    const {tokenId} = req.body;
+    client.verifyIdToken({idToken: tokenId, audience: process.env.GOOGLE_CLIENT_ID})
+    .then(response => {
+        const {email_verified, name, email} = response.getPayload;
+        console.log(response.payload);
     })
-    .then(isEqual => {
-        if(!isEqual) {
-            const error = new Error('Wrong Password!');
-            error.statusCode = 401;
-            throw error;
-        }
-        const token = jwt.sign(
-            {
-                email: loadedUser.email,
-                userId: loadedUser._id.toString()
-            },
-            'privatekey',
-            { expiresIn:'12h' }
-        );
-        res.status(200).json({token: token,name: loadedUser.name , userId: loadedUser._id.toString()});
-    })
-    .catch(err => {
-        if(!err.statusCode) {
-            err.statusCode = 500;
-        }
-        next(err);
-    })
+    // const email = req.body.email;
+    // const password = req.body.password;
+    // let loadedUser;
+    // User.findOne({ email: email })
+    // .then(user => {
+    //     if(!user) {
+    //         const error = new Error('User already exist');
+    //         error.statusCode(401);
+    //         throw error;
+    //     }
+
+    //     if(!user.isVerified) {
+    //         console.log('1233445566677787');
+    //         const otp = Math.floor(1000 + Math.random() * 9000).toString();
+    //         console.log(otp);
+
+    //         Otp.findOneAndRemove({ userId: user.id }, err => next(err));
+    //         console.log(user);
+    //         const userOtp = new Otp({
+    //             otp: otp,
+    //             userId: user._id
+    //         })
+    //         userOtp.save();
+
+    //         transporter.sendMail ({
+    //             to: email,
+    //             from: 'eventsity@india.com',
+    //             subject: 'Welcometo eventsity! Confirm your email',
+    //             html: `<h1>Thanks for signing up with Eventsity</h1>
+    //                     <h4>Here is your otp - ${otp}</h4>`
+    //         });
+
+    //         setTimeout(() => {
+    //             Otp.findByIdAndRemove(userOtp._id, err => next(err))
+    //         }, 200000);
+
+    //         console.log(user.id);
+    //         const error = new Error("User is not verified")
+    //         error.userId = user.id;
+    //         error.statusCode = 401;
+    //         throw error; 
+    //     }
+
+    //     loadedUser = user;
+    //     return bcrypt.compare(password, user.password);
+    // })
+    // .then(isEqual => {
+    //     if(!isEqual) {
+    //         const error = new Error('Wrong Password!');
+    //         error.statusCode = 401;
+    //         throw error;
+    //     }
+    //     const token = jwt.sign(
+    //         {
+    //             email: loadedUser.email,
+    //             userId: loadedUser._id.toString()
+    //         },
+    //         'privatekey',
+    //         { expiresIn:'12h' }
+    //     );
+    //     res.status(200).json({token: token,name: loadedUser.name , userId: loadedUser._id.toString()});
+    // })
+    // .catch(err => {
+    //     if(!err.statusCode) {
+    //         err.statusCode = 500;
+    //     }
+    //     next(err);
+    // })
 }
