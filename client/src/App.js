fe3ef1d691dbc3e4556649b74ecca05f60599c3f
tapixhx <tapish818@gmail.com>
2021-02-09 14:13:41 +0530

    Project setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center text-uppercase"> Hello! </h1>
+    </div>
+  );
+}
+
+export default App;
