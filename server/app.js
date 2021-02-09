69912063af1246e1705db3c1781e18b55f306fdf
tapixhx <tapish818@gmail.com>
2021-02-09 14:39:59 +0530

    Models, routes and controllers

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
+
+app.use('/auth', authRoutes);
 
 app.listen(8080);
---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const userSchema = new schema({
+    name : {
+        type:String,
+        required:true
+    },
+    email : {
+        type:String,
+        required : true
+    },
+    password : {
+        type:String,
+        required:true
+    },
+    status : {
+        type:String,
+        default: 'I am new',
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+})
+
+module.exports = mongoose.model('User',userSchema);
